0f134199a7fa974276ce165c114510de6f17f8af
Logan <monday.hopscotch@gmail.com>
2020-10-02 20:24:04 -0600

    update init

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-haxelib install haxefmod 
+haxelib install haxefmod
 haxelib git bitlytics https://github.com/bitDecayGames/Bitlytics.git
 haxelib git textpop https://github.com/bitDecayGames/text-pop.git
+haxelib git flixelutils https://github.com/bitDecayGames/flixel-utils.git
