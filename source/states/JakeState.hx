18f2d105f5a69232d8c6b45a6b0cd805f293917e
Jake Caban-Tomski <jacque006@gmail.com>
2020-10-03 15:39:01 -0600

    Combine lvlz

--- source/states/JakeState.hx
+++ source/states/JakeState.hx
@@ -2,19 +2,24 @@ package states;
 
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
-import events.BeatEvent;
-import events.RenderEvent;
+import flixel.FlxSprite;
+import flixel.input.actions.FlxAction.FlxActionDigital;
+import flixel.FlxG;
 import entities.Ship;
 import haxefmod.FmodEvents.FmodCallback;
+import flixel.FlxState;
+import actions.Actions;
+import events.BeatEvent;
+import events.RenderEvent;
 import flixel.system.FlxAssets;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import level.LevelIO;
-import level.Level;
 
-class JakeState extends FlxState
-{
+using extensions.FlxObjectExt;
+
+class JakeState extends FlxState {
+
 	// TODO: These wil likely live somewhere else ultimately
 	var bpm = 112.0; // hardcode these for now, but we could ideally get them from FMOD (but not for this jam)
 	var pixPerBeat = 100;
@@ -26,21 +31,42 @@ class JakeState extends FlxState
 
 	var currentBeat:Int = 0;
 
-	var debugSpeaker:FlxSprite;
-
 	var beatEvents:Array<BeatEvent> = [];
 	var renderEvents:Map<Int, Array<BeatEvent>> = new Map();
 
 	var beaters:Array<Ship> = [];
 	var tweens:Array<FlxTween> = [];
 
+	// milliseconds the player can be early
+	var bufferMS:Float = 100;
+
+	// milliseconds the player can be late
+	var postBufferMS:Float = 100;
+
+	var actions:Actions;
+	var inputBuffer:Map<FlxActionDigital, Float> = new Map();
+
+	var beatTime:Float;
+	var beatAwaitingProcessing:Bool;
+
+	var player:Ship;
+	var playerLane:Int = 0;
+
+	var laneCoords:Array<Float> = [
+		FlxG.width / 2 - 160,
+		FlxG.width / 2 - 80,
+		FlxG.width / 2,
+		FlxG.width / 2 + 80,
+		FlxG.width / 2 + 160
+	];
+
+	var debugSpeaker:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
 		FmodManager.PlaySong(FmodSongs.LetsGo);
 		FmodManager.RegisterCallbacksForSong(beat, FmodCallback.TIMELINE_BEAT);
-
-		var level = LevelIO.loadFromJSON(AssetPaths.level_0__json);
 
 		// Debug for testing purposes
 		beatEvents.push(new BeatEvent(15, 0.5, new Ship(0, 0)));
@@ -55,31 +81,17 @@ class JakeState extends FlxState
 		beatEvents.push(new BeatEvent(16, 3.0, new Ship(300, 0)));
 		parse(beatEvents);
 
-		var newLevel = new Level();
-		newLevel.name = "New level who dis";
-		newLevel.beatEvents = beatEvents;
-		LevelIO.saveToJSON(AssetPaths.level_0__json, newLevel);
+		actions = new Actions();
+
+		player = new Ship(0, 0);
+		alignPlayerToLane();
+		add(player);
 
 		debugSpeaker = new FlxSprite(FlxG.width / 2, FlxG.height / 2);
 		debugSpeaker.loadGraphic(AssetPaths.speakerTest__png, true, 80, 80);
 		debugSpeaker.animation.add("sound", [1, 0], 10, false);
 		debugSpeaker.animation.play("sound");
 		add(debugSpeaker);
-
-		#if !FLX_NO_DEBUG
-		var y = 0;
-		while (y < FlxG.height) {
-			var divider = new FlxSprite(FlxG.width / 2, y, AssetPaths.divider__png);
-			divider.scale.set(FlxG.width / divider.width, 1);
-			add(divider);
-			if (y == focusBeat * pixPerBeat) {
-				divider.alpha = 1;
-			} else {
-				divider.alpha = 0.5;
-			}
-			y += pixPerBeat;
-		}
-		#end
 	}
 
 	private function parse(events:Array<BeatEvent>) {
@@ -91,6 +103,8 @@ class JakeState extends FlxState
 
 			//              the impact y-coord  minus   how many beats on screen   times  how fast our ship moves
 			e.sprite.y = (focusBeat * pixPerBeat) - (e.impactBeat - beginRenderBeat) * pixPerBeat * e.speed;
+			// we want things to be lined up based on the bottom of the ship
+			e.sprite.y -= e.sprite.height;
 			e.sprite.startY = e.sprite.y;
 			if (!renderEvents.exists(beginRenderBeat)) {
 				renderEvents[beginRenderBeat] = new Array<BeatEvent>();
@@ -100,9 +114,11 @@ class JakeState extends FlxState
 		}
 	}
 
-
 	private function beat() {
 		debugSpeaker.animation.play("sound", true);
+		beatTime = Date.now().getTime();
+		beatAwaitingProcessing = true;
+
 		currentBeat++;
 
 		// cancel any in-progress tweens
@@ -137,19 +153,91 @@ class JakeState extends FlxState
 		}
 	}
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-		FmodManager.Update();
+	private function checkInput(timestamp:Float):Bool {
+		if (inputBuffer.exists(actions.left)) {
+			if (timestamp - inputBuffer[actions.left] < bufferMS) {
+				playerLane--;
+				player.color = FlxColor.GREEN;
+			} else {
+				player.color = FlxColor.RED;
+			}
+			resetBeatVars();
+		}
+
+		if (inputBuffer.exists(actions.right)) {
+			if (timestamp - inputBuffer[actions.right] < bufferMS) {
+				playerLane++;
+				player.color = FlxColor.GREEN;
+			} else {
+				player.color = FlxColor.RED;
+			}
+			resetBeatVars();
+		}
+
+		// return false if still awaiting processing
+		return !beatAwaitingProcessing;
 	}
 
-	 override public function onFocusLost():Void {
-		 super.onFocusLost();
-		 FmodManager.PauseSong();
-	 }
+	private function resetBeatVars() {
+		inputBuffer.clear();
+		alignPlayerToLane();
+		beatAwaitingProcessing = false;
+	}
 
-	 override public function onFocus():Void {
-		 super.onFocus();
-		 FmodManager.UnpauseSong();
-	 }
+	private function alignPlayerToLane() {
+		player.setMidpoint(laneCoords[playerLane], FlxG.height - 50);
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+		var timestamp = Date.now().getTime();
+		FmodManager.Update();
+
+		if (beatAwaitingProcessing) {
+			if (checkInput(beatTime)) {
+				// input was handled, just bail
+				return;
+			}
+
+			if (actions.left.check()) {
+				playerLane--;
+				player.color = FlxColor.YELLOW;
+				beatAwaitingProcessing = false;
+				resetBeatVars();
+			}
+
+			if (actions.right.check()) {
+				playerLane++;
+				player.color = FlxColor.YELLOW;
+				beatAwaitingProcessing = false;
+				resetBeatVars();
+			}
+
+			if (beatAwaitingProcessing && timestamp - beatTime > postBufferMS) {
+				// hasn't processed beat yet, AND it's been past the buffer
+				// count is as missed and move on
+				resetBeatVars();
+			}
+		} else {
+			if (actions.left.check() && !inputBuffer.exists(actions.left)) {
+				inputBuffer[actions.left] = timestamp;
+				player.color = FlxColor.BLUE;
+			}
+
+			if (actions.right.check() && !inputBuffer.exists(actions.right)) {
+				inputBuffer[actions.right] = timestamp;
+				player.color = FlxColor.BLUE;
+			}
+		}
+	}
+
+	override public function onFocusLost():Void {
+		super.onFocusLost();
+		FmodManager.PauseSong();
+	}
+
+	override public function onFocus():Void {
+		super.onFocus();
+		FmodManager.UnpauseSong();
+	}
 }
