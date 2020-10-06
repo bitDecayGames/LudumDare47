4cfe29533d3b61378ba6b5346b22c119badb57c2
Logan <monday.hopscotch@gmail.com>
2020-10-06 12:02:32 -0600

    Fix fullscreen. add touch controls

--- source/actions/Actions.hx
+++ source/actions/Actions.hx
@@ -1,5 +1,8 @@
 package actions;
 
+import flixel.input.actions.FlxActionInputDigital.FlxActionInputDigitalIFlxInput;
+import flixel.FlxObject;
+import flixel.input.FlxSwipe;
 import flixel.input.keyboard.FlxKey;
 import flixel.input.actions.FlxAction.FlxActionDigital;
 
@@ -13,18 +16,22 @@ class Actions {
 	public function new() {
 		up.addKey(FlxKey.W, JUST_PRESSED);
 		up.addKey(FlxKey.UP, JUST_PRESSED);
+		up.add(new SwipeInputDigital(FlxObject.UP));
 		up.addGamepad(DPAD_UP, JUST_PRESSED);
 
 		down.addKey(FlxKey.S, JUST_PRESSED);
 		down.addKey(FlxKey.DOWN, JUST_PRESSED);
+		down.add(new SwipeInputDigital(FlxObject.DOWN));
 		down.addGamepad(DPAD_DOWN, JUST_PRESSED);
 
 		left.addKey(FlxKey.A, JUST_PRESSED);
 		left.addKey(FlxKey.LEFT, JUST_PRESSED);
+		left.add(new SwipeInputDigital(FlxObject.LEFT));
 		left.addGamepad(DPAD_LEFT, JUST_PRESSED);
 
 		right.addKey(FlxKey.D, JUST_PRESSED);
 		right.addKey(FlxKey.RIGHT, JUST_PRESSED);
+		right.add(new SwipeInputDigital(FlxObject.RIGHT));
 		right.addGamepad(DPAD_RIGHT, JUST_PRESSED);
 	}
 }
